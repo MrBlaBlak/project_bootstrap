61507d4989948182df94fe4a6f1355369bceb108
Michał Romak <mrblablak@gmail.com>
2017-07-01 16:51:18 +0200

    nav_działa

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 'use strict';
-$(function(){
-   $('.carousel').carousel({
-       interval:1000
-   });
-});
+
+$(function () {
+    $(window).scroll(function () {
+            if($(window).scrollTop()) {
+                document.getElementById("main-nav").style.backgroundColor="rgba(14, 216, 214, 0.9)";
+
+            }
+        else document.getElementById("main-nav").style.backgroundColor="rgba(0, 0, 0, 0)";
+    });
+            });
