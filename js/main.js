964f1b8fdb1247839c60964478e8e2bce14a67c8
Michał Romak <mrblablak@gmail.com>
2017-07-02 12:50:31 +0200

    portfolio_początek

--- js/main.js
+++ js/main.js
@@ -1,11 +1,53 @@
 'use strict';
 
+function scroll(){
+    if ($(window).scrollTop()) {
+        document.getElementById("main-nav").style.backgroundColor = "rgba(14, 216, 214, 0.9)";
+    } else {
+        document.getElementById("main-nav").style.backgroundColor = "rgba(0, 0, 0, 0)";
+    } 
+}
 $(function () {
-    $(window).scroll(function () {
-            if($(window).scrollTop()) {
-                document.getElementById("main-nav").style.backgroundColor="rgba(14, 216, 214, 0.9)";
 
-            }
-        else document.getElementById("main-nav").style.backgroundColor="rgba(0, 0, 0, 0)";
+    scroll();
+
+    $(window).scroll(function () {
+        scroll();
     });
-            });
+
+
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+});
